bc14ea5ac154520b9da412cb920843b2a9fc724e
iantendu <iantendu@gmail.com>
2022-02-02 23:24:09 +0300

    Finished Meditation UI

---
+++ app/src/main/java/com/plcoding/meditationuiyoutube/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.plcoding.meditationuiyoutube
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import com.plcoding.meditationuiyoutube.ui.theme.HomeScreen
+import com.plcoding.meditationuiyoutube.ui.theme.MeditationUIYouTubeTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
